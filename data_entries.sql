c711d84b7bee139fa215a679514b4843e13d4ff3
DiwanBhattarai <diwanbhattarai999@gmail.com>
2026-07-08 01:33:10 +0545

    Push

--- data_entries.sql
+++ data_entries.sql
@@ -1,4 +1,5 @@
 INSERT INTO users (name,email,password) VALUES
 -- ("deewan","diwan101@gmail.com","diwan123"),
 -- ("harsh","harsh500@gmail.com","harsh100"),
-("karan","karan100@gmail.com","karan123")
+-- ("karan","karan100@gmail.com","karan123"),
+("chandan","chandan789@gmail.com","chandan789")
